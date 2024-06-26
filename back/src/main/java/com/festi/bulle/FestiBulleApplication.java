fda680ff405282fcfac18a4f410139537fe8b3fc
Bastos Gomes Helder <helder@teamr.eu>
2024-06-26 11:47:28 +0200

    first back commit

---
+++ back/src/main/java/com/festi/bulle/FestiBulleApplication.java
@@ -0,0 +1,15 @@
+package com.festi.bulle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class FestiBulleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestiBulleApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/festi/bulle/Exercice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festi.bulle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
